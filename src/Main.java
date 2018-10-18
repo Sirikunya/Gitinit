596f9a0bddab45328eb6e5c6c19e975d3dcc2506
Sirikunya <fill9029f@gmail.com>
2018-10-18 12:11:35 +0700

    main new

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-	Scanner Sc = new Scanner(System.in);
-	String name = Sc.nextLine();
-	
-	Hello hello = new Hello();
-	hello.setName(name);
-	System.out.print(getName);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Scanner Sc = new Scanner(System.in);
+			String name = Sc.nextLine();
+			Hello hello = new Hello();
+			hello.setName(name);
+			System.out.print(hello.getName());
+		
+
+	}
+
 }
