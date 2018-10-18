197aeaa19180294afa75007dd2a60d0faea5e1ff
Sirikunya <fill9029f@gmail.com>
2018-10-18 12:02:11 +0700

    main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	Scanner Sc = new Scanner(System.in);
+	String name = Sc.nextLine();
+	
+	Hello hello = new Hello();
+	hello.setName(name);
+	System.out.print(getName);
 }
